a666eba75e623863ee73b847355eb553a3771b31
ezulkosk <ezulkosk@gsd.uwaterloo.ca>
2017-05-10 14:48:21 -0400

    script

--- scripts/compute_simp_timings.sh
+++ scripts/compute_simp_timings.sh
@@ -1,67 +1,67 @@
 #!/bin/bash
 maple=$1
 bench=$2
-in_dir=~/backdoors_benchmarks/${bench}/cnf/
+file=$3
+
 out_dir=~/backdoors_benchmarks/${bench}/simp_test/
 
 mkdir $out_dir
 
-for file in ${in_dir}/*.cnf
+
+name=`basename $file .cnf`
+out_file=${out_dir}/${name}.simp_test
+rm -f $out_file
+echo $file
+for elim in "-elim" "-no-elim"
 do
-    name=`basename $file .cnf`
-    out_file=${out_dir}/${name}.simp_test
-    rm -f $out_file
-    echo $file
-    for elim in "-elim" "-no-elim"
+    if [[ $elim == "-elim" ]]
+    then
+	elim_str="1 "
+    else
+	elim_str="0 "
+    fi
+    for asymm in "-asymm" "-no-asymm"
     do
-	if [[ $elim == "-elim" ]]
+	if [[ $asymm == "-asymm" ]]
 	then
-	    elim_str="1 "
+	    asymm_str="1 "
 	else
-	    elim_str="0 "
+	    asymm_str="0 "
 	fi
-	for asymm in "-asymm" "-no-asymm"
+	for rcheck in "-rcheck" "-no-rcheck"
 	do
-	    if [[ $asymm == "-asymm" ]]
+	    if [[ $rcheck == "-rcheck" ]]
 	    then
-		asymm_str="1 "
+		rcheck_str="1 "
 	    else
-		asymm_str="0 "
+		rcheck_str="0 "
 	    fi
-	    for rcheck in "-rcheck" "-no-rcheck"
+	    for sublim in " " "-sub-lim=0"
 	    do
-		if [[ $rcheck == "-rcheck" ]]
+		if [[ $sublim == " " ]]
 		then
-		    rcheck_str="1 "
-		else
-		    rcheck_str="0 "
-		fi
-		for sublim in " " "-sub-lim=0"
-		do
-		    if [[ $sublim == " " ]]
-		    then
-			sublim_str="1 "
+		    sublim_str="1 "
 		    else
 			sublim_str="0 "
-		    fi
-		    if [[ $elim == "-no-elim" && $asymm == "-no-asymm" && $rcheck == "-no-rcheck" && $sublim == "-sub-lim=0" ]]
-		    then
-			#echo -no-pre $file
-			res=`$maple -no-pre $file | grep "Simplifi\|CPU time"`
-		    else
-			#echo $elim $asymm $rcheck $sublim $file
-			res=`$maple $elim $asymm $rcheck $sublim $file | grep "Simplifi\|CPU time"`			
-		    fi
-		    res=`echo $res | tr '|' ' '`
-		    simp_time=`echo $res | awk '{print $3}'`
-		    total_time=`echo $res | awk '{print $8}'`
-		    # echo $simp_time
-		    # echo $total_time
-		    solving_time=`echo $total_time $simp_time | awk '{print $1-$2}'`
-		    echo $elim_str $asymm_str $rcheck_str $sublim_str $simp_time $solving_time $total_time
-		    echo $elim_str $asymm_str $rcheck_str $sublim_str $simp_time $solving_time $total_time >> $out_file
-		done
+		fi
+		if [[ $elim == "-no-elim" && $asymm == "-no-asymm" && $rcheck == "-no-rcheck" && $sublim == "-sub-lim=0" ]]
+		then
+		    #echo -no-pre $file
+		    res=`$maple -no-pre $file | grep "Simplifi\|CPU time"`
+		else
+		    #echo $elim $asymm $rcheck $sublim $file
+		    res=`$maple $elim $asymm $rcheck $sublim $file | grep "Simplifi\|CPU time"`			
+		fi
+		res=`echo $res | tr '|' ' '`
+		simp_time=`echo $res | awk '{print $3}'`
+		total_time=`echo $res | awk '{print $8}'`
+		# echo $simp_time
+		# echo $total_time
+		solving_time=`echo $total_time $simp_time | awk '{print $1-$2}'`
+		echo $elim_str $asymm_str $rcheck_str $sublim_str $simp_time $solving_time $total_time
+		echo $elim_str $asymm_str $rcheck_str $sublim_str $simp_time $solving_time $total_time >> $out_file
 	    done
 	done
     done
 done
+
