246f365015dd49ddfac1cfb88649beef4b3660b3
ezulkosk <ezulkosk@gsd.uwaterloo.ca>
2017-05-14 00:11:50 -0400

    script

--- scripts/compute_weak_empty.sh
+++ scripts/compute_weak_empty.sh
@@ -2,12 +2,14 @@
 
 maplesat=$1
 openwbo=$2
-bench=$3
-instance=$4
-num_iters=1000
+#bench=$3
+fmeasy_dir=$3
+out_dir=$4
+instance=$5
+num_iters=$6
 
 
-out_dir=/home/ezulkosk/backdoors_benchmarks/${bench}/weak_e_files/
+#out_dir=/home/ezulkosk/backdoors_benchmarks/${bench}/weak_e_files/
 mkdir $out_dir
 
 name=`basename $instance .cnf`
@@ -15,13 +17,13 @@ rm ${out_dir}/${name}.weak_empty
 for seed in `seq 1 $num_iters`
 do
     ${maplesat}  ${instance} ${out_dir}/${name}.${seed}.model  -no-pre -rnd-seed=$seed -rnd-init -rnd-freq=1 -rnd-pol
-    python3 /home/ezulkosk/fmeasy/src/hitting_set_generator.py $input ${out_dir}/${name}.${seed}.model > ${out_dir}/${name}.${seed}.wcnf
+    python3 ${fmeasy_dir}/src/hitting_set_generator.py $instance ${out_dir}/${name}.${seed}.model > ${out_dir}/${name}.${seed}.wcnf
     ${openwbo} ${out_dir}/${name}.${seed}.wcnf > ${out_dir}/${name}.${seed}.result
-    cat ${name}.${seed}.result | grep "^v" | sed 's/v //g' | tr ' ' '\n' | grep -c "-" >> ${out_dir}/${name}.weak_empty
+    cat ${out_dir}/${name}.${seed}.result | grep "^v" | sed 's/v //g' | tr ' ' '\n' | grep -c "-" >> ${out_dir}/${name}.weak_empty
     rm ${out_dir}/${name}.${seed}.wcnf ${out_dir}/${name}.${seed}.result ${out_dir}/${name}.${seed}.model 
     
 done
 
-cat ${out_dir}/${name}.*.weak_empty | sort -rn | head -n 1 > ${out_dir}/${name}.best_weak_empty
+
 
 
